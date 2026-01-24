bd2107980da5dd8404ac1595f5d91476053f0cad
zoikle <134870626+Gros26@users.noreply.github.com>
2026-01-23 19:01:00 -0500

    added 03 exercise

---
+++ 02. JavaScript/03. Save to localStorage/index.js
@@ -0,0 +1,6 @@
+// Save a value to localStorage
+localStorage.setItem('username', 'Alice');
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+const username = localStorage.getItem('username');
+console.log(username); 
