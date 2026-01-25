8f98e10f44f66d1f0758d2e1e9fe0117b1c0559a
zoikle <134870626+Gros26@users.noreply.github.com>
2026-01-25 17:05:47 -0500

    An icon was added to indicate whether the price went up, down, or stayed the same

--- 03. Advanced JS Foundations/04. Challenge: Stock Ticker/index.js
+++ 03. Advanced JS Foundations/04. Challenge: Stock Ticker/index.js
@@ -4,7 +4,9 @@ const stockDisplayName = document.getElementById("name")
 const stockDisplaySymbol = document.getElementById("symbol")
 const stockDisplayPrice = document.getElementById("price")
 const stockDisplayTime = document.getElementById("time")
-
+let oldPrice = 0
+let newPrice
+let toAdd
 
 renderStockTicker(getStockData())
 
@@ -12,13 +14,15 @@ function getFreshStockData() {
     const freshData = getStockData()
     renderStockTicker(freshData)
 }
-
+ 
 
 function renderStockTicker(stockData) {
+    newPrice = stockData.price
     stockDisplayName.textContent = `Name: ${stockData.name}`
     stockDisplaySymbol.textContent = `Symbol: ${stockData.symbol}`
-    stockDisplayPrice.textContent = `Price: ${stockData.price}`
+    toAdd = oldPrice > newPrice ? "📉" : oldPrice < newPrice ? "📈" : "▶️"
+    stockDisplayPrice.textContent = `Price: ${stockData.price} ${toAdd}`
     stockDisplayTime.textContent = `Time: ${stockData.time}`
-
+    oldPrice = newPrice
     setTimeout(getFreshStockData, 1500)
 }
