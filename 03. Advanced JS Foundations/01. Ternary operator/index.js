e968dbf1313635b08549913390cf00200bd38c11
zoikle <134870626+Gros26@users.noreply.github.com>
2026-01-25 12:14:18 -0500

    added a ternary operator most complex

--- 03. Advanced JS Foundations/01. Ternary operator/index.js
+++ 03. Advanced JS Foundations/01. Ternary operator/index.js
@@ -1,5 +1,9 @@
 const exerciseTimeMins = 20
+const exerciseTimeMins2 = 60
 
-let message = exerciseTimeMins < 30 ? "Doing good!" : "You need to try harder!"
+const message = exerciseTimeMins < 30 ? "Doing good!" : "You need to try harder!"
+
+const message2 =  exerciseTimeMins2 < 30 ? "Doing good!" : exerciseTimeMins2 < 60 ? "You need to try harder!" : "Super bad"
 
 console.log(message)
+console.log(message2)
