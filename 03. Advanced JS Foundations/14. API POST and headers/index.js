4fd7bdf764d7858cc5e2a93b85ba12d0c2f47154
zoikle <134870626+Gros26@users.noreply.github.com>
2026-01-26 21:56:14 -0500

    added an example about api method post and headers to tells the server the content type

--- 03. Advanced JS Foundations/14. API POST and headers/index.js
+++ 03. Advanced JS Foundations/14. API POST and headers/index.js
@@ -2,7 +2,17 @@ const url = "https://jsonplaceholder.typicode.com/posts"
 
 async function getData(url) {
     try {
-        const response = await fetch(url)
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: new Headers({
+                'Content-Type': 'application/json' // Tells the server the body is JSON
+            }),
+            body: JSON.stringify({
+                title: 'Holiday Nightmares',
+                body: 'When I was kidnapped in Scotland...' ,
+                id: 1,
+            })
+        })
         console.log(response.status)
         if (!response.ok) {
             throw new Error('Error: '+ response.status)
